c9f558a654764a3f58153c5a69f9ee5788093209
Kevinuyeh <isrealkevin929@gmail.com>
2025-09-14 17:07:20 -0400

    new commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,42 +12,67 @@
 
 //   return (
 //     <div>
-//       <nav className="bg-dark3 shadow-md flex items-center justify-between w-full z-50  p-3">
+//       {/* Navbar fixed at top */}
+//       <nav className="bg-dark3 shadow-md flex items-center justify-between fixed top-0 w-full z-50 p-5">
 //         {/* Title aligned to the left */}
 //         <div className="flex items-center mx-8">
 //           <Link to="/" className="flex items-center">
 //             <IoCodeSlashSharp size={30} className="text-white mr-2" />
-//             <span className="text-white font-galada text-lg md:text-xl">Iconic Vision</span>
+//             <span className="text-white font-galada text-lg md:text-xl">
+//               Iconic Vision
+//             </span>
 //           </Link>
 //         </div>
 
 //         {/* Hamburger Icon for mobile */}
 //         <span onClick={handleNav} className="md:hidden block cursor-pointer">
-//           {nav ? <AiOutlineClose size={25} color="white" /> : <AiOutlineMenu size={25} color="white" />}
+//           {nav ? (
+//             <AiOutlineClose size={25} color="white" />
+//           ) : (
+//             <AiOutlineMenu size={25} color="white" />
+//           )}
 //         </span>
 
-//         {/* Navigation items - pushed to the right */}
+//         {/* Navigation items */}
 //         <ul
 //           className={`${
-//             nav ? 'left-0' : '-left-full'
+//             nav ? "left-0" : "-left-full"
 //           } md:static absolute md:mx-8 top-3 md:top-12 md:flex md:items-center bg-dark3 md:bg-transparent w-[75%] md:w-auto h-screen md:h-auto p-5 md:p-0 transition-all duration-500 ease-in-out md:ml-auto`}
 //         >
+          
 //           <li className="text-white text-xl p-2">
-//             <Link to="/" onClick={() => setNav(false)}>Home</Link>
+//             <Link to="/" onClick={() => setNav(false)}>
+//               Home
+//             </Link>
 //           </li>
 //           <li className="text-white text-xl p-2">
-//             <Link to="/about" onClick={() => setNav(false)}>About</Link>
+//             <Link to="/about" onClick={() => setNav(false)}>
+//               About
+//             </Link>
+//           </li>
+//                     <li className="text-white text-xl p-2">
+//             <Link to="/projectpage" onClick={() => setNav(false)}>
+//               Projects
+//             </Link>
 //           </li>
 //           <li className="text-white text-xl p-2">
-//             <Link to="/contact" onClick={() => setNav(false)}>Contact</Link>
+//             <Link to="/contact" onClick={() => setNav(false)}>
+//               Contact
+//             </Link>
 //           </li>
 //         </ul>
 //       </nav>
+
+//       {/* Spacer div to prevent hero/content from being hidden under navbar */}
+//       <div className="h-[70px]"></div>
 //     </div>
 //   );
 // }
 
 // export default Navbar;
+
+
+
 
 
 
@@ -94,25 +119,32 @@ function Navbar() {
         <ul
           className={`${
             nav ? "left-0" : "-left-full"
-          } md:static absolute md:mx-8 top-3 md:top-12 md:flex md:items-center bg-dark3 md:bg-transparent w-[75%] md:w-auto h-screen md:h-auto p-5 md:p-0 transition-all duration-500 ease-in-out md:ml-auto`}
+          } md:static absolute md:mx-8 top-0 md:top-12 md:flex md:items-center bg-dark3 md:bg-transparent w-[75%] md:w-auto h-screen md:h-auto p-5 md:p-0 transition-all duration-500 ease-in-out md:ml-auto`}
         >
-          
-          <li className="text-white text-xl p-2">
+          {/* Show Title above nav links on mobile */}
+          <div className="md:hidden mb-6 flex items-center">
+            <IoCodeSlashSharp size={30} className="text-white mr-2" />
+            <span className="text-white font-galada text-2xl">
+              Iconic Vision
+            </span>
+          </div>
+
+          <li className="text-white text-xl p-2 border-b border-gray-600 md:border-none">
             <Link to="/" onClick={() => setNav(false)}>
               Home
             </Link>
           </li>
-          <li className="text-white text-xl p-2">
+          <li className="text-white mt-5 md:mt-0 text-xl p-2 border-b border-gray-600 md:border-none">
             <Link to="/about" onClick={() => setNav(false)}>
               About
             </Link>
           </li>
-                    <li className="text-white text-xl p-2">
+          <li className="text-white mt-5 md:mt-0 text-xl p-2 border-b border-gray-600 md:border-none">
             <Link to="/projectpage" onClick={() => setNav(false)}>
               Projects
             </Link>
           </li>
-          <li className="text-white text-xl p-2">
+          <li className="text-white mt-5 md:mt-0 text-xl p-2 border-gray-600 md:border-none">
             <Link to="/contact" onClick={() => setNav(false)}>
               Contact
             </Link>
